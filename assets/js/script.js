e362fcb6b0652fb5f3b126ee27d7a171b67b59d4
aspart5623y <willemzy2002@gmail.com>
2021-10-30 22:13:34 +0100

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+/* ========================  P R E L O A D E R  ======================= */
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', () => {
+        ready();
+    });
+}
+
+
+function ready() { 
+    
+    
+
+
+
+
+}
